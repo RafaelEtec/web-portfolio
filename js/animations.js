9cdb12bad71fd538eab44c97a48df85b3ba365c9
rafael.goulart <rafaelxms@hotmail.com>
2024-06-05 14:56:05 -0300

    added transitions and better icon placements

--- js/animations.js
+++ js/animations.js
@@ -3,5 +3,32 @@ splashScreen.addEventListener('click',()=>{
   splashScreen.style.opacity = 0;
   setTimeout(()=>{
     splashScreen.classList.add('hidden')
-  },610)
+  },800)
 })
+setTimeout(()=>{
+  splashScreen.style.opacity = 0;
+  setTimeout(()=>{
+    splashScreen.classList.add('hidden')
+  }, 800)
+}, 3200)
+
+
+const anim = anime.timeline({
+  loop: false,
+  direction: 'alternate',
+});
+
+anim
+.add({
+  targets: '#hexagon path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutQuart',
+  duration: 2000,
+  delay: function(el, i) { return i * 250 },
+})
+.add({
+  targets: '#hexagon #R',
+  duration: 1000,
+  opacity: 1,
+  easing: 'easeInOutQuart'
+});
