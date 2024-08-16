b087b1270849b357aa28bf2953323b50105d0e7a
Rafael Goulart <rafaelxms@hotmail.com>
2024-08-15 21:58:12 -0300

    new project

--- js/infos/projects.js
+++ js/infos/projects.js
@@ -4,14 +4,20 @@ var portfolio = {
 };
 var infosec = {
     titulo: "InfoSec - Guardiões da noite",
-    descricao: "Protótipo de aplicativo de controle defuncionários, destinado a profissionais de segurança de bairro noturnos. Elaborado em grupo como projeto de final de curso. Acreditamos no avanço da ideia"
+    descricao: "Protótipo de aplicativo de controle de funcionários, destinado a profissionais de segurança de bairro noturnos. Elaborado em grupo como projeto de final de curso. Acreditamos no avanço da ideia"
 };
+
+var go_game = {
+    titulo: "Jogo em Golang",
+    descricao: "A fim de aprender a linguagem GO, decidi criar um jogo no estilo Temple Run"
+};
+
 var hackthebox = {
     titulo: "HackTheBox Machines Tutorial",
     descricao: "Elaboração de um guia em formato de página web ensinando a completar os desafios da plataforma de Hacking"
 };
 
-const projetos = {portfolio, infosec, hackthebox};
+const projetos = {portfolio, go_game, hackthebox};
 
 new Vue({
     el: "#col-projetos",
