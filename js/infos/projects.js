ef99e8ecb39235e7d0c296096ed04e36a0c5379d
Rafael Goulart <rafaelxms@hotmail.com>
2025-03-24 12:36:31 -0300

    added images to projects

--- js/infos/projects.js
+++ js/infos/projects.js
@@ -2,7 +2,8 @@ var portfolio = {
     titulo: "Portfólio WEB",
     descricao: {
         'https://github.com/RafaelEtec/web-portfolio': "Website pessoal construído com HTML, CSS, JavaScript, Vue.js, Anime.js e P5.js, atualmente hospedado no GitHub Pages"
-    }
+    },
+    img: "",
 };
 
 var go_game = {
@@ -13,14 +14,16 @@ var go_game = {
         'https://github.com/RafaelEtec/go_sudoku': "SUDOKU",
         'https://github.com/RafaelEtec/go_tictactoe': "Jogo da Velha",
         'https://github.com/RafaelEtec/go_game': "RPG"
-    }
+    },
+    img: "",
 };
 
 var go_api = {
     titulo: "APIs RESTful",
     descricao: {
         'https://github.com/RafaelEtec/parking-lot': "Estacionamento - Golang - Gin - MySQL"
-    }
+    },
+    img: "",
 };
 
 var java = {
@@ -28,21 +31,24 @@ var java = {
     descricao: {
         'https://github.com/RafaelEtec/PI4': "E-Commerce - Disciplina Musical",
         'https://github.com/RafaelEtec/Jogo_da_Velha': "Jogo da Velha"
-    }
+    },
+    img: "",
 };
 
 var react = {
     titulo: "Aplicações em React Native",
     descricao: {
         'https://github.com/RafaelEtec/segue-o-lider': "Segue o Líder - Este projeto React Native representa um marco no meu aprendizado sobre desenvolvimento mobile. Criado para armazenar a pontuação de jogadores em partidas presenciais, explora tecnologias como Expo, Appwrite e Nativewind. Conta com autenticação segura, navegação dinâmica, lista de amigos, upload de mídia e um design responsivo. Mais que uma aplicação funcional, é um estudo prático que consolidou meus conhecimentos na criação de apps móveis multiplataforma."
-    }
+    },
+    img: "/docs/images/segueolider.png",
 }
 
 var next = {
     titulo: "Aplicações em Next.js",
     descricao: {
         'https://github.com/RafaelEtec/bookstorm': "BookStorm - É uma plataforma moderna e escalável. Permite empréstimo de livros com notificações automáticas, gestão de usuários e análise de dados. Conta com uma interface intuitiva para usuários e um painel administrativo completo. Utiliza tecnologias avançadas para garantir desempenho, segurança e automação."
-    }
+    },
+    img: "/docs/images/bookstorm.png",
 }
 
 const projetos = {portfolio, react, next, go_game};
